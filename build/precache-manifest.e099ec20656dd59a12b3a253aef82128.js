95e6ec94c40ffbe340dfa65984fa5f71c5d83897
Mallaiya M <mallaiyazap@gmail.com>
2018-11-18 13:47:09 +0530

    initial commit

---
+++ build/precache-manifest.e099ec20656dd59a12b3a253aef82128.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "8c561bbe8120ca453702",
+    "url": "/Mallaiya/FlooringStore.git/static/css/main.931adc83.chunk.css"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "c62e3ddc484d8fd5aec8",
+    "url": "/Mallaiya/FlooringStore.git/static/js/1.c62e3ddc.chunk.js"
+  },
+  {
+    "revision": "74337febe5ecbe396224",
+    "url": "/Mallaiya/FlooringStore.git/static/js/runtime~main.74337feb.js"
+  },
+  {
+    "revision": "ac840491f70c8fac4c0398128edb0be4",
+    "url": "/Mallaiya/FlooringStore.git/static/media/collection-1.ac840491.jpg"
+  },
+  {
+    "revision": "00c8b4f1114f4dbf45dbd9c225ef2a87",
+    "url": "/Mallaiya/FlooringStore.git/static/media/collection-2.00c8b4f1.jpg"
+  },
+  {
+    "revision": "00067b8dd1ae10de604d0d48c6b5fe69",
+    "url": "/Mallaiya/FlooringStore.git/static/media/collection-3.00067b8d.jpg"
+  },
+  {
+    "revision": "27a60d30685bd7af8ce6a195ddd035c9",
+    "url": "/Mallaiya/FlooringStore.git/static/media/banner.27a60d30.jpg"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "8c561bbe8120ca453702",
+    "url": "/Mallaiya/FlooringStore.git/static/js/main.8c561bbe.chunk.js"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/Mallaiya/FlooringStore.git/static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "c62e3ddc484d8fd5aec8",
+    "url": "/Mallaiya/FlooringStore.git/static/css/1.83d6c3f3.chunk.css"
+  },
+  {
+    "revision": "617006fe2d5b33b0e4fab6b6d07fad1d",
+    "url": "/Mallaiya/FlooringStore.git/index.html"
+  }
+];
